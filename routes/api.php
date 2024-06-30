2bfb16b231be97135315d56baaa2f38d12c9a146
MahmoudNagah999 <Mahmoudnagah999@gmail.com>
2024-06-30 10:12:32 +0300

    middleware

--- routes/api.php
+++ routes/api.php
@@ -25,4 +25,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('/products', ProductController::class)->middleware('auth:sanctum');
+Route::resource('/products', ProductController::class)->middleware(['auth:sanctum', 'adminCheck:admin']);
